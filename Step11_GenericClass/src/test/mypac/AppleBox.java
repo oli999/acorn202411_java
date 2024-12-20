d36bcd5ebb02f138d11df4cf4cc464cc5b08abee
kimgura <oli999@naver.com>
2024-12-20 15:40:18 +0900

    Step11 프로젝트 추가함

---
+++ Step11_GenericClass/src/test/mypac/AppleBox.java
@@ -0,0 +1,16 @@
+package test.mypac;
+
+public class AppleBox {
+	//필드
+	private Apple item;
+	
+	//사과를 포장하는 메소드
+	public void pack(Apple item) {
+		this.item=item;
+	}
+	
+	//사고를 꺼내는 메소드 
+	public Apple unPack() {
+		return item;
+	}
+}
