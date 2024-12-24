b08051e8b6d223301647d575a43cce520f6c0a86
kimgura <oli999@naver.com>
2024-12-24 12:20:39 +0900

    Step13_Exception 프로젝트 추가함

---
+++ Step13_Exception/src/test/main/MainClass05.java
@@ -0,0 +1,12 @@
+package test.main;
+
+public class MainClass05 {
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("main 메소드가 시작 되었습니다.");
+		
+		//스레드를 일정시간 지연 시키기( mili second 단위로 숫자를 전달하면 된다 )
+		Thread.sleep(1000*5);
+		
+		System.out.println("main 메소드가 종료 됩니다.");
+	}
+}
